cdc22346fa411ed75655c5399481da0729cb47fb
Ajinstellus <ajinstellus07@gmail.com>
2025-10-02 15:56:29 +0530

    initial comment

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js frontend!');
+});
+
+app.listen(port, () => {
+  console.log(`Frontend running on http://localhost:${port}`);
+});
+
